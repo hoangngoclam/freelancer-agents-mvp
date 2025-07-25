f8978fa29094cb195643dd6ab21e6bbd442c6f30
truonghoangngoclam <truong.lam@marketenterprise.vn>
2025-07-25 23:14:50 +0700

    fix run astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@tailwindcss/vite';
+// @ts-check
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite"
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
-    plugins: [tailwind()]
+    plugins: [tailwindcss()],
   },
-  output: 'static'
-});
+})
